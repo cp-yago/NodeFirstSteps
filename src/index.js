8f5db9874e64ddf33132b240e1f2a7700fe4d1a7
Yago Cunha <yago.cunha123@gmail.com>
2020-06-15 22:11:32 -0300

    Usando parametros

--- src/index.js
+++ src/index.js
@@ -4,8 +4,45 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello Yagoooooo!'})
+//informar para o app que ele irá entende o formato json
+app.use(express.json())
+
+app.get('/projects', (request, response) => {
+  const { title, owner } = request.query
+  
+  console.log(title, owner)
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ])
+})
+
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body
+  console.log(title, owner)
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+})
+
+app.put('/projects/:id', (request, response) => {
+  const {id} = request.params
+  console.log(id)
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 2',
+    'Projeto 3',
+  ])
 })
 
 app.listen(3334, () => {
